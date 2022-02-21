92765e3cedfd0f35e20961ef25d67b94c0219e3a
chenqian7 <chenqian7@joyy.com>
2022-02-21 17:37:31 +0800

    添加framework

---
+++ common.framework/Headers/A.h
@@ -0,0 +1,19 @@
+//
+//  A.h
+//  Alamofire
+//
+//  Created by 陈谦 on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+#import "B.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface A : NSObject
+
+@property (nonatomic, strong) B *b;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ common.framework/Headers/B.h
@@ -0,0 +1,21 @@
+//
+//  B.h
+//  common
+//
+//  Created by 陈谦 on 2022/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+#define SCBTEST 1.0
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SCTestB : NSObject
+@end
+
+@interface B : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
