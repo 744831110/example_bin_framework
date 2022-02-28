d6403361c42af7b0cc9eedca6d968b27f82e2f7e
chenqian7 <chenqian7@joyy.com>
2022-02-28 19:53:51 +0800

    添加common_depend.framework

---
+++ common_depend/0.1.0/common_depend.framework/Headers/CDTest.h
@@ -0,0 +1,16 @@
+//
+//  CDTest.h
+//  Pods
+//
+//  Created by 陈谦 on 2022/2/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CDTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
